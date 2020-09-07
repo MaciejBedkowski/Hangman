688eaeb9106001cd06ebbde9268cc8cb55a01386
Maciej <mathiass222@gmail.com>
2020-09-07 18:18:43 +0200

    Perm Check program

---
+++ PermCheck/PermCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PermCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 4,1,3,2};
+            int permNumber = 0;
+            int check = 1;
+
+            for (int i = 0; i<A.Length; i++)
+            {
+                if(A[i]==check)
+                {
+                    check++;
+                    WriteLine("Table:" + A[i]);
+                    i = -1;
+                    
+                    WriteLine("Check:"+check);
+                    WriteLine(i);
+                }
+            }
+            if(check == A.Length+1)
+            {
+                permNumber = 1;
+            }
+            
+            ReadKey();
+        }
+    }
+}
